e9cdc2a82bb4bd8398571f541a5d88ab0997becc
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-10 22:16:22 -0300

    Installing gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://lucasgithubx.github.io/card-react/todo-typescript"
 })
